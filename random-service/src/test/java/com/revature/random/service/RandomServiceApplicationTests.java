d87eeebe8616873a6a0df909007751a4dc4bb80a
Patrick Runyan <jpwrunyan@hotmail.com>
2017-11-12 19:44:00 -0500

    random get service

---
+++ random-service/src/test/java/com/revature/random/service/RandomServiceApplicationTests.java
@@ -0,0 +1,18 @@
+package com.revature.random.service;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RandomServiceApplicationTests {
+
+	@Ignore
+	@Test
+	public void contextLoads() {
+	}
+
+}
